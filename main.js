2cc6b1ab095c8b47a833b3fdc84772631f1f06bc
Alexsandro <alexrogeriocarv@outlook.com>
2023-08-23 18:21:10 -0300

    Realizado a Alteração no JS e HTML

--- main.js
+++ main.js
@@ -1,59 +1,42 @@
-const form = document.getElementById('form-valid');
-const limparButton = document.getElementById('limparButton');
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('validaForm');
 
-let numeroA = parseFloat(document.getElementById('campoA').value);
-let numeroB = parseFloat(document.getElementById('campoB').value);
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
 
+        const campoA = parseFloat(document.getElementById('campoA').value);
+        const campoB = parseFloat(document.getElementById('campoB').value);
 
-let formValido = true;
+        function validacaoFormulario(campoA, campoB) {
+            return campoB >= campoA;
+        }
 
-function campoValido(campoB, campoA) {
-    return campoB > campoA;
-}
+        const mensagemSucesso = `Positivo, o valor de B: ${campoB} é maior que o Valor de A: ${campoA}`;
+        const mensagemErro = `Inválido, o valor de B: ${campoB} é inferior que o Valor de A: ${campoA}`;
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+        if (validacaoFormulario(campoA, campoB)) {
+            const cssMensagemSucesso = document.querySelector('.success-message');
+            cssMensagemSucesso.innerHTML = mensagemSucesso;
+            cssMensagemSucesso.style.display = 'block';
+        } else {
+            const cssMensagemErro = document.querySelector('.error-message');
+            cssMensagemErro.innerHTML = mensagemErro;
+            cssMensagemErro.style.display = 'block';
+        }
+    });
 
-    const mensagemSucesso = `O valor do Campo B: ${campoB.value} é maior que o numero do Campo A: ${campoA.value} `;
-    const mensagemErro = `Resultado Inválido o valor de B: ${campoB.value} é menor que o valor de A: ${campoA.value}`;
+    const limparButton = document.getElementById('limparButton');
 
-    formValido = campoValido(campoA.value, campoB.value);
-    if (!formValido) {
-        const containerMenssagemSucesso = document.querySelector('.success-message');
-        containerMenssagemSucesso.innerHTML = mensagemSucesso;
-        containerMenssagemSucesso.style.display = 'block';
-
-        /*const cssMensagemSucesso = document.querySelector('.success-messagem');
+    limparButton.addEventListener('click', function() {
+        const cssMensagemSucesso = document.querySelector('.success-message');
         cssMensagemSucesso.innerHTML = '';
-        cssMensagemSucesso.style.display = 'none';*/
+        cssMensagemSucesso.style.display = 'none';
 
-        campoA.value = '';
-        campoB.value = '';
-
-    } else {
-        const containerMenssagemErro = document.querySelector('.message-error');
-        containerMenssagemErro.innerHTML = mensagemErro;
-        containerMenssagemErro.style.display = 'block';
-
-        const cssMensagemErro = document.querySelector('.message-error');
+        const cssMensagemErro = document.querySelector('.error-message');
         cssMensagemErro.innerHTML = '';
         cssMensagemErro.style.display = 'none';
 
         document.getElementById('campoA').value = '';
         document.getElementById('campoB').value = '';
-    }
-
+    });
 });
-/*limparButton.addEventListener('click', function () {
-    const cssMensagemSucesso = document.querySelector('.success-messagem');
-    cssMensagemSucesso.innerHTML = '';
-    cssMensagemSucesso.style.display = 'none';
-
-    const cssMensagemErro = document.querySelector('.message-error');
-    cssMensagemErro.innerHTML = '';
-    cssMensagemErro.style.display = 'none';
-
-    document.getElementById('campoA').value = '';
-    document.getElementById('campoB').value = '';
-
-});*/
